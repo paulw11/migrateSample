7ea7f72f9b9f870a2a05b34ec819c2bec7cb3627
Paul Wilkinson <paulw@wilko.me>
2017-09-28 13:22:26 +1000

    Initial Commit

---
+++ migrateSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  migrateSample
+//
+//  Created by Paul Wilkinson on 28/9/17.
+//  Copyright © 2017 Paul Wilkinson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
